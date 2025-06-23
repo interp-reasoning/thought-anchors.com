510f7a552280ab3aa961684b9548c7e3d484751c
Uzay Macar <uzay.macar@gmail.com>
2025-06-23 12:18:13 -0700

    Update to new title.

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'principled-attribution.com',
+                hostname: 'thought-anchors.com',
                 port: '',
             },
         ],
--- src/constant/navbar.js
+++ src/constant/navbar.js
@@ -1,14 +1,14 @@
 export const navbarItems = [
     {
         name: 'Paper Code',
-        href: 'https://github.com/uzaymacar/principled-attribution',
+        href: 'https://github.com/uzaymacar/thought-anchors',
     },
     {
         name: 'Interface Code',
-        href: 'https://github.com/uzaymacar/principled-attribution.com',
+        href: 'https://github.com/uzaymacar/thought-anchors.com',
     },
     {
         name: 'Paper',
-        href: 'https://github.com/uzaymacar/principled-attribution',
+        href: 'https://github.com/uzaymacar/thought-anchors',
     },
 ]
